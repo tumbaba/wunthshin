e261b7a790e35c9fcfe39686b715d130ce2e0366
Colibri Shin <colibrishin@gmail.com>
2024-10-15 13:55:40 +0900

    chore: initial third person template project (#1)
    초기 기본 템플릿 프로젝트 푸싱
    
    - 3인칭 템플릿
    - 애니메이션 팩 추가함

---
+++ Source/wunthshin/wunthshinGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "wunthshinGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AwunthshinGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AwunthshinGameMode();
+};
+
+
+
