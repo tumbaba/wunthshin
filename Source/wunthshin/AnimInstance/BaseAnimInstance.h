a360065e0ad3d9acf0f48d64860c2e4ca827ce37
SILVERCRIMSON\f6d33 <f6d333@gmail.com>
2024-10-16 16:12:17 +0900

    - 케릭터 모션 및  animinstance 생성

---
+++ Source/wunthshin/AnimInstance/BaseAnimInstance.h
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimInstance.h"
+#include "BaseAnimInstance.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UBaseAnimInstance : public UAnimInstance
+{
+	GENERATED_BODY()
+
+public:
+	virtual void NativeInitializeAnimation() override;
+	virtual void NativeUpdateAnimation(float DeltaSeconds) override;
+
+protected:
+	UPROPERTY()
+	class UPawnMovementComponent* MovementComponent;
+
+	UPROPERTY(BlueprintReadOnly)
+	double WalkSpeed = 0.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	double RunSpeed = 0.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	double FastRunSpeed = 0.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bShoudWalk = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bShoudRun = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bShoudFastRun = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsCrouch = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsFalling = false;
+
+	// 스테미나하고 연동 필요
+	UPROPERTY(BlueprintReadOnly)
+	bool bStaminaEmpty = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bIsCliming = false;
+
+	// 벽높이 상대적인 높이?
+	UPROPERTY()
+	double WallHight = 0.0f;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bwallHightLow = false;
+
+	UPROPERTY(BlueprintReadOnly)
+	bool bwallHightmid = false;
+;
+	UPROPERTY(BlueprintReadOnly)
+	bool bwallHighthHigh = false;
+};
